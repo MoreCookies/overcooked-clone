8bf80b8f728c66ef470000cffc3059bd2b43af1e
Yana <msyanajain@gmail.com>
2026-03-09 09:42:16 -0700

    animation

--- dialogue.js
+++ dialogue.js
@@ -124,8 +124,8 @@ function mouseClicked() {
             "You, tRNA, are the chief. \nYou’ll pick up ingredients and carry them across from station to station. Every order has 3 ingredients (the codons)  that make up the dish (1 amino acid).",
             "When you hand a dish to the counter (P site), you swap your appearance;\n you’re a new tRNA ready for the dish! The A site is where you accept the next dish recipe (mRNA).",
             "You’ll have a single giant plate for the whole order. That’s your start codon… place it first before cooking. \nThen follow the mRNA ticket exactly: ingredient by ingredient.",
-            "You have 3:00 minutes. When the timer hits 0:00 (stop codon), the monster will roar (release factor). \nFinish in time or… 😨",
-            "Now get to it! \nYour monsters are hungry, time is running, and Potato King IV is counting on you!"
+            "You have 3:00 minutes. When you finish dish #3 (stop codon), the monster will roar (release factor). \n If they roar, they’re happy! If they don’t, they get hangry and the world will burn.😨 \n",
+            "Now get to it! \nYour monsters are waiting and Potato King IV is counting on you!"
           ]);
         } else {
           text_bubble = null;
